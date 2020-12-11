ea82b3cf4e895c63e592b5c781a84343da0305b0
psgaiduk <65785004+psgaiduk@users.noreply.github.com>
2020-12-11 14:01:27 +0300

    add test_get_doc_shelf

--- 04_adpy/04_tests/task_1.py
+++ 04_adpy/04_tests/task_1.py
@@ -27,11 +27,18 @@ class TestSuit:
 
     @pytest.mark.parametrize("test_input,expected", [
         ({"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
-            'invoice "11-2" "Геннадий Покемонов"'),
+         'invoice "11-2" "Геннадий Покемонов"'),
         ({"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
-            'passport "2207 876234" "Василий Гупкин"'),
+         'passport "2207 876234" "Василий Гупкин"'),
         ({"type": "insurance", "number": "10006", "name": "Аристарх Павлов"},
-            'insurance "10006" "Аристарх Павлов"')])
+         'insurance "10006" "Аристарх Павлов"')])
     def test_show_document_info(self, test_input, expected):
         assert app.show_document_info(test_input) == expected
 
+    @patch('builtins.input')
+    @pytest.mark.parametrize("test_input,expected", [(["11-2"], '1'),
+                                                     (["2207 876234"], '1'),
+                                                     (["10006"], '2')])
+    def test_get_doc_shelf(self, m_input, test_input, expected):
+        m_input.side_effect = test_input
+        assert app.get_doc_shelf() == expected
