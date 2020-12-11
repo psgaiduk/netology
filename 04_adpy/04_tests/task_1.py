5cff23cb09c4288819d7b8b35166293927bff249
psgaiduk <65785004+psgaiduk@users.noreply.github.com>
2020-12-11 13:36:28 +0300

    add test_get_doc_owner_name

---
+++ 04_adpy/04_tests/task_1.py
@@ -0,0 +1,20 @@
+from unittest.mock import patch
+
+import app
+import pytest
+
+
+class TestSuit:
+    @patch('builtins.input')
+    def test_secretary_program_start_end(self, m_input):
+        m_input.side_effect = ['q']
+        assert app.secretary_program_start() is None
+
+    @patch('builtins.input')
+    @pytest.mark.parametrize("test_input,expected", [(["11-2"], "Геннадий Покемонов"),
+                                                     (["2207 876234"], "Василий Гупкин"),
+                                                     (["10006"], "Аристарх Павлов")])
+    def test_get_doc_owner_name(self, m_input, test_input, expected):
+        m_input.side_effect = test_input
+        assert app.get_doc_owner_name() == expected
+
